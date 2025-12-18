f1840f8f1a427d08752b7632d66dd2c82a92e899
AmineDouis <aminedouiss2004@gmail.com>
2025-12-18 15:40:07 +0100

    Revert "merged gestion des taches"
    This reverts commit cb47c5aecc21f5f8bc10e3bacc8e88a39cbd3a09.

--- EmployeeApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ajouter/', views.ajouter_employe, name='ajouter_employe'),
-    path('liste/', views.liste_employes, name='liste_employes'),
-]
--- GestionTacheApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestiontacheappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GestionTacheApp'
